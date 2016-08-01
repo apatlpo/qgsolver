e1cfe88bad9c2134fd1b4d037e727c74e44073ac
aurelien <aurelien.junk@gmail.com>
2016-08-01 16:51:32 +0200

    Add reading capibility for q and for nonvector variables

--- dev/test_basic.py
+++ dev/test_basic.py
@@ -12,16 +12,25 @@ from qgsolver.io import write_nc
 
 if __name__ == "__main__":
     
-    qg = qg(grid_uniform_input = {'Nx':300, 'Ny':200, 'Nz':3 },
+    qg = qg(grid_uniform_input = {'Nx':150, 'Ny':100, 'Nz':3 },
             K = 0.e0, dt = 0.1*86400.e0)
     #
     qg.set_q()
     qg.invert_pv()
     write_nc([qg.PSI, qg.Q], ['psi', 'q'], 'output.nc', qg)
     #
-    if False:
+    test=1
+    if test==0:
+        # one time step and store
         qg.tstep(1)
         write_nc([qg.PSI, qg.Q], ['psi', 'q'], 'output.nc', qg, create=False)
+    elif test==1:
+        # write/read/write
+        qg.tstep(1)
+        write_nc([qg.PSI, qg.Q], ['psi', 'q'], 'output1.nc', qg, create=True)
+        qg.set_q(file_q='output.nc')
+        qg.tstep(1)
+        write_nc([qg.PSI, qg.Q], ['psi', 'q'], 'output1.nc', qg, create=False)
     else:
         while qg.tstepper.t/86400. < 10 :
             qg.tstep(1)
