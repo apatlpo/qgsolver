4a0705e768aa4b7bdb89b321fd4f967bd1bf6d9f
Aurelien @LPO <aurelien.junk@gmail.com>
2017-12-20 14:25:54 +0100

    udpate env to fix API

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name='qgsolver',
+      version='0.0.0',
+      description='Quasi-Geostrophic solver',
+      url='https://github.com/apatlpo/qgsolver',
+      author='Aurelien Ponte',
+      author_email='aurelien.junk@gmail.com',
+      license='',
+      packages=['qgsolver'],
+      install_requires=['petsc4py', 'numpy', 'scipy']
+      )
