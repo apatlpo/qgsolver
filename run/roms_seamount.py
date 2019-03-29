478d13320e26f8f8fd1a559499b9bfcfa42f43a0
Aurelien PONTE <aponte@datarmor2.ib0.ice.ifremer.fr>
2019-03-29 17:01:56 +0000

    add code to analyze roms seamount case

--- run/roms_seamount.py
+++ run/roms_seamount.py
@@ -38,15 +38,13 @@ def roms_input_runs(ncores_x=4, ncores_y=6, ping_mpi_cfg=False):
         cur_time = start_time
         
         # Top and Bottom boundary condition type: 'N' for Neumann, 'D' for Dirichlet
-        bdy_type = {'top': 'N_PSI', 'bottom': 'N_PSI', 'periodic': True}
+        bdy_type = {'top': 'N_PSI', 'bottom': 'N_PSI'}
 
         # vertical subdomain
         vdom = {'Nz': 48}
 
         # horizontal subdomain
         hdom = {'Nx': 100, 'Ny': 150}
-        # 256 = 2^8
-        # 720 = 2^4 x 3^2 x 5
 
         datapath = '../input/'
         outdir = '../output/'
@@ -54,63 +52,30 @@ def roms_input_runs(ncores_x=4, ncores_y=6, ping_mpi_cfg=False):
         vgrid = datapath+'roms_metrics.nc'
         file_q = datapath+'roms_pv.nc'
         file_psi = datapath+'roms_psi.nc'
-        #file_rho = datapath+'roms_rho.nc'
         file_bg = datapath+'roms_bg.nc'
-               
-        qg = qg_model(ncores_x=ncores_x, ncores_y=ncores_y,
-                      hgrid=hgrid, vgrid=vgrid, vdom=vdom, hdom=hdom, mask=True,
-                      boundary_types=bdy_type,
-                      f0N2_file=file_q, K=2000.e0, dt=0.02 * d2s,
-                      verbose=1)
+
+        params = {'ncores_x': ncores_x, 'ncores_y' :ncores_y,
+                  'hgrid': hgrid, 'vgrid': vgrid, 'vdom': vdom, 'hdom': hdom, 
+                  'mask': True, 'f0N2_file': file_q,
+                  'K': 2000.e0, 'dt': 0.02 * d2s,
+                  'verbose': 1}
+
+        ##               
+        qg = qg_model(boundary_types=bdy_type, **params)
 
         # start filling in variables    
         qg.set_q(file=file_q)
         qg.set_psi(file=file_psi)   
-        #qg.set_rho(file=file_rho)
-        #qg.write_state(filename=outdir+'output0.nc')
         qg.write_state(filename=outdir+'input.nc')
         # substract background state
         bstate = qg.set_bstate(file=file_bg)
-        
-        add(qg.state,bstate,da=None,a2=-1.)
+        add(qg.state, bstate, da=None, a2=-1.)
         #qg.state += -bstate
         qg.write_state(filename=outdir+'input.nc', append=True)
         # after PV inversion
         qg.invert_pv()
-        qg.write_state(filename=outdir+'input.nc', append=True)
+        qg.write_state(filename=outdir+'output0.nc')
 
-        ## compute CFL
-        #CFL = qg.compute_CFL()
-        #if qg._verbose>0: print('CFL='+str(CFL))
-                
-        #
-        test=-1
-        if test==0:
-            #Should be updated
-            # one time step and store
-            #qg.tstep(1, rho_sb=True, bstate=bstate)
-            #qg.write_state(filename=outdir+'output.nc', append=True)
-            pass
-        elif test==1:
-            #Should be updated
-            #while qg.tstepper.t/86400. < 200 :
-            #    qg.tstep(50, rho_sb=True, bstate=bstate)
-            #    qg.write_state(filename=outdir+'output.nc', append=True)
-            pass
-        elif test==2:
-            Ndays = 100.   # in days
-            dt_out = .5    # in days
-            #
-            idx=1
-            di = int(dt_out * d2s/qg.tstepper.dt)
-            while qg.tstepper.t/d2s < Ndays :
-                qg.write_state(filename=outdir+'output_%.3i.nc'%idx)
-                #
-                qg.tstep(di, rho_sb=True, bstate=bstate)
-                KE = qg.compute_KE()
-                if qg._verbose>0: print(' KE = %.6e' %KE)
-                idx+=1
-        
         if qg._verbose>0:
             print('----------------------------------------------------')
             print('Elapsed time for all ',str(time.time() - cur_time))
